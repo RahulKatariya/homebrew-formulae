3eb65b75ab716e5a8ea537ba1075b25c650dd797
Rahul Katariya <rahulkatariya@me.com>
2016-09-13 22:33:38 +0530

    Update aarkay.rb

--- Formula/aarkay.rb
+++ Formula/aarkay.rb
@@ -2,7 +2,7 @@ class Aarkay < Formula
   homepage "http://rahulkatariya.me/"
   head "https://github.com/rahulkatariya/aarkay_cli"
   url "https://github.com/rahulkatariya/aarkay_cli/archive/0.0.1/aarkay_cli.tar.gz"
-  sha256 "69d2ccbf9889d91f2e352f5774d2e3ee9f1b2f86b1c6f3bdbe578e3128415edd"
+  sha256 "78af0f7fa99764a78e9b69bd6f6251101c21b56198108e3271add88ac528167f"
 
   def install
     system "make" if build.head?
