5074664c0fe55d0c5b9ed196ffb4064fac328816
Rahul Katariya <rahulkatariya@me.com>
2016-09-13 22:43:43 +0530

    Update aarkay.rb

--- Formula/aarkay.rb
+++ Formula/aarkay.rb
@@ -1,7 +1,7 @@
 class Aarkay < Formula
   homepage "http://rahulkatariya.me/"
   head "https://github.com/rahulkatariya/aarkay_cli"
-  url "https://github.com/rahulkatariya/aarkay_cli/archive/0.0.1/aarkay_cli.tar.gz"
+  url "https://github.com/rahulkatariya/aarkay_cli/archive/0.0.1.tar.gz"
   sha256 ""
 
   def install
