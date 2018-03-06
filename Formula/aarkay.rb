054c717513aa48d395ec387e20008d51fe56aa8d
Rahul Katariya <rahulkatariya@me.com>
2018-03-06 18:01:10 +0530

    Update Homebrew Formulae

--- Formula/aarkay.rb
+++ Formula/aarkay.rb
@@ -1,10 +1,11 @@
 class Aarkay < Formula
-  homepage "http://rahulkatariya.me/"
-  head "https://github.com/rahulkatariya/aarkay_cli"
-  url "https://github.com/rahulkatariya/aarkay_cli/archive/0.0.2.tar.gz"
-  sha256 ""
+  desc "A command line utility to create multiple files by mapping data to template"
+  homepage "https://rahulkatariya.me"
+  url "https://github.com/RahulKatariya/AarKay/releases/download/v0.0.1/AarKay-v0.0.1.zip"
+  version "0.0.1"
+  sha256 "9b1e46af5950789ae55ab7b98df7a09bca00849405adfb548e0f5472fb96afee"
 
   def install
-    system "make", "DESTDIR=#{prefix}", "install"
+    bin.install "aarkay"
   end
 end
