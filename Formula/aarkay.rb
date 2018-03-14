f7547272c4628849ab7c9516221217ff4be47b18
Rahul Katariya <rahulkatariya@me.com>
2018-03-15 01:18:13 +0530

    Update aarkay.rb

--- Formula/aarkay.rb
+++ Formula/aarkay.rb
@@ -3,17 +3,19 @@ class Aarkay < Formula
   homepage "https://rahulkatariya.me"
   url "https://github.com/RahulKatariya/AarKay/releases/download/v0.0.2/AarKay-v0.0.2.zip"
   version "0.0.2"
-  sha256 "9ff180b72a30fceea02a4669ae617600ab2da43dfbbb5dfa0f83d9e46a4df691"
+  sha256 "f73da4ee260255e80e7533fd049c27086d766165f39f224547b1c6092700aab1"
 
   def install
     bin.install "aarkay"
   end
   
   def caveats; <<~EOS 
+=====================================================
 For Upgrading Users -
       
 AarKay Root Directory changed from ~/Developer to ~/
 AarKayFiles Directory renamed to AarKayData    
+=====================================================
 EOS
   end
 
