53fb084fcb207b95ea72a785796c30e363072229
Rahul Katariya <rahulkatariya@me.com>
2018-04-02 00:01:21 +0530

    Update aarkay.rb

--- Formula/aarkay.rb
+++ Formula/aarkay.rb
@@ -1,9 +1,9 @@
 class Aarkay < Formula
-  desc "A command line utility to create multiple files by mapping data to template"
+  desc "A code generation framework for all languages"
   homepage "https://rahulkatariya.me"
-  url "https://github.com/RahulKatariya/AarKay/releases/download/v0.0.3/AarKay-v0.0.3.zip"
-  version "0.0.3"
-  sha256 "fc6d2a4e9fd6183ecb0e4c130294280820e5301791596c16888a11dfd30a18d8"
+  url "https://github.com/RahulKatariya/AarKay/releases/download/v0.0.4/AarKay-v0.0.4.zip"
+  version "0.0.4"
+  sha256 "39ed7f3450dacda1092d014481d4025d2c93514858a119487ce03acb991e2228"
 
   def install
     bin.install "aarkay"
