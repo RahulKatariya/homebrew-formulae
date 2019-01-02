d5d51cef49ff44118bf79d76c8fa001ecbc09289
Rahul Katariya <rahulkatariya@me.com>
2019-01-02 20:06:34 +0530

    Update aarkay.rb

--- Formula/aarkay.rb
+++ Formula/aarkay.rb
@@ -3,7 +3,7 @@ class Aarkay < Formula
   homepage "https://aarkay.xyz"
   url "https://github.com/RahulKatariya/AarKay/releases/download/v0.0.13/AarKay-v0.0.13.zip"
   version "0.0.13"
-  sha256 "4ddb46340828b7434b318f7aeaeb73bc63e17a9067103774840932a76b462e34"
+  sha256 "221474fee8e2753de2486dad126ba9ff5f902c56f63a8bf2a4fafe5c43bd54ba"
 
   def install
     bin.install "aarkay"
