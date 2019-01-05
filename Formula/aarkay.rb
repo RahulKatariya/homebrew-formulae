f7a0228beb27252709dbdae01c589538c4321ab5
Rahul Katariya <rahulkatariya@me.com>
2019-01-05 15:03:42 +0530

    Update aarkay.rb

--- Formula/aarkay.rb
+++ Formula/aarkay.rb
@@ -1,9 +1,9 @@
 class Aarkay < Formula
   desc "A language independent code generation framework"
   homepage "https://aarkay.xyz"
-  url "https://github.com/RahulKatariya/AarKay/releases/download/v0.0.17/AarKay-v0.0.17.zip"
-  version "0.0.17"
-  sha256 "fdcc658f11f464e836d329bdcee624e3ffe62fe31fc65d859851afc6171a5f45"
+  url "https://github.com/RahulKatariya/AarKay/releases/download/v0.0.18/AarKay-v0.0.18.zip"
+  version "0.0.18"
+  sha256 "268518634685f890a67337c52cb2ac2896e405bf6899a250919dba6bf7794d24"
 
   def install
     bin.install "aarkay"
